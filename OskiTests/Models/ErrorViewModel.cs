f251d2f7929255cf47bd12ab7cfcedd31979a165
Ilya Kotomin <mostlys4nny@gmail.com>
2022-12-06 19:58:38 +0200

    Добавьте файлы проекта.

---
+++ OskiTests/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OskiTests.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
