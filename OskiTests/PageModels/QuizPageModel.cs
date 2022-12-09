338d5b5827f0548147740d03ae3b3b69bfd6c918
Ilya Kotomin <mostlys4nny@gmail.com>
2022-12-09 07:19:30 +0200

    Returned to last commit

---
+++ OskiTests/PageModels/QuizPageModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using OskiTests.QuizzesAPI;
+
+namespace OskiTests.PageModels
+{
+    public class QuizPageModel : PageModel
+    {
+        public RandomQuiz? Quiz { get; set; }
+
+        public IActionResult OnGet(string passedObject)
+        {
+            Quiz = JsonConvert.DeserializeObject<RandomQuiz>(passedObject);
+
+            if (Quiz == null)
+                return NotFound();
+
+            return Page();
+        }
+
+
+    }
+}
